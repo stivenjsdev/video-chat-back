d50cf16bb2d78684bbd20702a3b11895cfa01e28
Sensei Stiven <strujillodev@gmail.com>
2024-05-23 22:53:53 -0500

    check if room exists on server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const http = require('http');
-const { v4: uuidv4 } = require('uuid');
-const cors = require('cors');
-const twilio = require('twilio');
+const express = require("express");
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
+const twilio = require("twilio");
 
 const PORT = process.env.PORT || 5002;
 
@@ -12,14 +12,28 @@ const server = http.createServer(app);
 
 app.use(cors());
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
+let connectedUsers = [];
+let rooms = [];
+
+// create route to check if room exists
+app.get("/api/room-exists/:roomId", (req, res) => {
+  const { roomId } = req.params;
+  const room = rooms.find((room) => room.id === roomId);
+
+  if (room) {
+    // send response that room exists
+  } else {
+    // send response that room does not exists
   }
+});
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 server.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
-
